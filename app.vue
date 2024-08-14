e6e3e499628eaaee44d0b35aa6952479eabfadb6
fenggp <18319407429@163.com>
2024-08-14 23:42:24 +0800

    hello world api

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1>{{ msg }}</h1>
 </template>
+<script setup="ts">
+const { data: msg } = await useFetch('http://api.fenggp.com.cn')
+</script>
